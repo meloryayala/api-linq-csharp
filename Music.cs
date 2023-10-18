f8e0ba1e9baa8acec400d9c4dce8f97ce7eefeeb
Melory Ayala <melory.ayala@gmail.com>
2023-10-18 18:15:18 +0200

    Consuming musics API

---
+++ Music.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+namespace music_box4.Models;
+
+public class Music
+{
+    [JsonPropertyName("song")]
+    public string? Name { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+    
+    [JsonPropertyName("duration_ms")]
+    public int Duration { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genre { get; set; }
+
+    public void DisplayMusicDetails()
+    {
+        Console.WriteLine($"Artist: {Artist}");
+        Console.WriteLine($"Music: {Name}");
+        Console.WriteLine($"Duration: {Duration /1000}s");
+        Console.WriteLine($"Genre: {Genre}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using music_box4.Models;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musics = JsonSerializer.Deserialize<List<Music>>(response)!;
+        musics[1998].DisplayMusicDetails();
+        Console.WriteLine(musics.Count);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"We have a problem {e}");
+    }
+}
